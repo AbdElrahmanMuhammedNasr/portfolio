2fcfaf3a61cc559afedb51c0864c4d18013f90eb
3bdo <34967675+AbdElrahmanMuhammedNasr@users.noreply.github.com>
2024-09-06 10:03:51 +0300

    first blog

--- blog/script.js
+++ blog/script.js
@@ -7,16 +7,18 @@ let posts = [
              { type: 'image', src: './asyncPic/1.png', alt: 'Placeholder Image' },
             { type: 'text', content: 'i will send 9 request at same time  we will find the the first 8 requests done at same time but the request number 9 wait 10 sec until it excute ' },
             { type: 'image', src: './asyncPic/2.png', alt: 'Placeholder Image' },
-            { type: 'text', content: 'this because of <strong>threadPoolTaskExecutorBuilder</strong> it makes' +
-                    ' 8 thread to about 2 billion threads and queue size is 2 billion this make request number 9 will wait because the 8 thread serve the first 8 requests' +
+            { type: 'text', content: 'this because of <strong>threadPoolTaskExecutorBuilder</strong> it makes  8 thread to about 2 billion threads and queue size is 2 billion ' +
+                    '<br/> this make request number 9 will wait because the 8 threads serve the first 8 requests ' +
                     'and will not create new thread because the queue is not full yet ' },
             { type: 'image', src: './asyncPic/3.png', alt: 'Placeholder Image' },
             { type: 'text', content: 'how can we  solve it'},
-            { type: 'text', content: 'crete custom threadPoolTaskExecutorBuilder'},
+            { type: 'text', content: 'create custom threadPoolTaskExecutorBuilder'},
             { type: 'image', src: './asyncPic/4.png', alt: 'Placeholder Image' },
             { type: 'text', content: 'and use the name of bean installed of leave  it  empty'},
             { type: 'image', src: './asyncPic/5.png', alt: 'Placeholder Image' },
-            { type: 'text', content: 'So when I sent 9 requests at the same time, the first 3 were executed at the same time because there were already 3 threads. The second 3 were added to the queue waiting. The third 3 did not find a place in the queue, so 3 new threads were created. When the first 3 finished their tasks, they went to the 3 in the queue and executed them.'},
+            { type: 'text', content: 'So when I sent 9 requests at the same time, the first 3 were executed at the same time because there were already 3 threads. ' +
+                    '<br/> The second 3 were added to the queue waiting. The third 3 did not find a place in the queue, so 3 new threads were created. ' +
+                    '<br/>When the first 3 finished their tasks, they went to the 3 in the queue and executed them.'},
             { type: 'image', src: './asyncPic/6.png', alt: 'Placeholder Image' },
 
         ]
@@ -39,7 +41,7 @@ function createSummary(summaryArray) {
         if (item.type === 'text') {
             return `<p>${item.content}</p>`;
         } else if (item.type === 'image') {
-            return `<img src="${item.src}" alt="${item.alt}" />`;
+            return `<img style="width: 50%" src="${item.src}" alt="${item.alt}" />`;
         }
     }).join(' ');
 }
@@ -51,6 +53,7 @@ function createBlogPost(title, date, summaryArray) {
     const summaryHTML = createSummary(summaryArray);
 
     post.innerHTML = `
+    <div class="all-post">
         <div class="post-header">
             <h3 class="post-title">${title}</h3>
             <p class="post-date">Posted on ${date}</p>
@@ -58,19 +61,17 @@ function createBlogPost(title, date, summaryArray) {
         <div class="summary-content" style="display: none;">
             <p>${summaryHTML}</p>
         </div>
-        <button class="toggle-button">Show Post</button>
-     `;
+      </div>  
+      `;
 
     // Add event listener to toggle the summary visibility
-    const toggleButton = post.querySelector('.toggle-button');
-    const summaryContent = post.querySelector('.summary-content');
-
-    toggleButton.addEventListener('click', () => {
+     const summaryContent = post.querySelector('.summary-content');
+    const allPost = post.querySelector('.all-post');
+    const postHeader = post.querySelector('.post-header');
+    postHeader.addEventListener('click', () => {
         const isVisible = summaryContent.style.display === 'block';
         summaryContent.style.display = isVisible ? 'none' : 'block';
-        toggleButton.textContent = isVisible ? 'Show Post' : 'Hide Post';
-    });
-
+     });
     return post;
 }
 
